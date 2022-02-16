107138b09de86e7f1cb6d28133d42e1144d77d11
Benson <bensonmakau2000@gmail.com>
2022-02-16 16:03:45 +0300

    restyled login with bootstrap 4

--- index.php
+++ index.php
@@ -4,78 +4,111 @@ include 'server.php';
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Maseno E-Help</title>
-    <link rel="icon" href="./static/img/logo.png" type="image/x-icon" />
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1, shrink-to-fit=no"
     />
-    <!--Bootstrap 4 CSS-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <!--Style Css-->
-    <link rel="stylesheet" href="./static/css/style.css" />
+    <link rel="icon" href="./static/img/logo.png" type="image/x-icon" />
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+
+    <title>Maseno | Login</title>
   </head>
   <body>
-    <div class="form-center">
-      <div class="container-fluid" id="login-form">
-        <div class="login-header">
-          <h2 class="login-heading">Maseno E-Help Login</h2>
-          <img src="./static/img/logo.png" class="logo" />
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 mt-5">
+          <div class="col-md-12 mb-2" style="background-color: #00a7d0">
+            <h2 class="text-center" style="color: #fff">Maseno E-Help Login</h2>
+
+            <div class="row">
+              <div class="col-md-3"></div>
+              <div class="col-md-6 text-center">
+                <img
+                  src="./static/img/logo.png"
+                  class="img-fluid"
+                  height="100"
+                  width="100"
+                />
+              </div>
+              <div class="col-md-3"></div>
+            </div>
+          </div>
+          <form method="post" action="" enctype="multipart/form-data">
+            <div class="form-group row">
+              <?php
+                include 'errors.php';
+                ?>
+              <label for="inputEmail3" class="col-sm-2 col-form-label"
+                >Username</label
+              >
+              <div class="col-sm-10">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="inputEmail3"
+                  placeholder="Username"
+                  name="regno"
+                />
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="inputPassword3" class="col-sm-2 col-form-label"
+                >Password</label
+              >
+              <div class="col-sm-10">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                  name="password"
+                />
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <div class="col-sm-12">
+                <button
+                  type="submit"
+                  name="login_btn"
+                  class="btn btn-primary btn-block"
+                >
+                  Sign in
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-        <form class="form-style" method="post" action="" enctype="multipart/form-data" >
-        <?php
-include 'errors.php';
-?>
-
-          <div class="form-group">
-            <label for="exampleInputEmail1" class="form-label"
-              >Registration Number</label
-            >
-            <input
-              type="text"
-              class="form-control input-length"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Registration Number"
-              name="regno"
-            />
-            <small id="emailHelp" class="form-text text-muted"
-              >We'll never share your credentials with anyone else.</small
-            >
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1" class="form-label"
-              >Password</label
-            >
-            <input
-              type="password"
-              class="form-control input-length"
-              id="exampleInputPassword1"
-              placeholder="Password"
-              name="password"
-            />
-          </div>
-
-          <button type="submit" name="login_btn"class="btn btn-primary login-btn">
-            Sign In
-          </button>
-          <hr />
-          <p class="account-p">
-            Don't have an account?
-            <a class="btn btn-success reg-btn" href="register.php">Register Here</a>
-          </p>
-        </form>
+        <div class="col-md-3"></div>
       </div>
     </div>
 
-    <!--Bootstrap 4 scripts-->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <!-- End Bootstrap 4 scripts-->
-
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script
+      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"
+    ></script>
     <?php
     
     // LOGIN USER
