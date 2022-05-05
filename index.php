05813bccc047ec585f9e9023d1dd8e7ffa6a7c43
Benson <bensonmakau2000@gmail.com>
2022-05-05 06:54:42 +0300

    updated documentation v0

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include './components/header.php';
             <div class="col-md-6 mt-5 login-page">
 
                 <div class="col-md-12 mb-2 login-page-header">
-                    <h2 class="text-center" style="color: #fff">Maseno E-Help Login</h2>
+                    <h2 class="text-center" style="color: #fff">Maseno University Emergency System</h2>
 
                     <div class="row">
                         <div class="col-md-3"></div>
@@ -33,7 +33,7 @@ include './components/header.php';
             ?>
                         <label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputEmail3" placeholder="Username" name="regno"
+                            <input type="text" class="form-control" id="inputEmail3" placeholder="e.g BA/00025/013" name="regno"
                                 required />
                         </div>
                     </div>
@@ -46,19 +46,25 @@ include './components/header.php';
                     </div>
 
                     <div class="form-group row">
-                        <div class="col-sm-12">
-                            <button type="submit" name="login_btn" class="btn btn-primary btn-block">
-                                Sign in
+                    <div class="col-sm-4">
+                            <button type="submit" name="forgot_password_btn" class="btn btn-warning btn-block p-2">
+                               Forgot Password ?
+                            </button>
+                        </div>
+                        <div class="col-sm-4"></div>
+                        <div class="col-sm-4">
+                            <button type="submit" name="login_btn" class="btn btn-primary btn-block p-2">
+                              <strong>  Sign In</strong>
                             </button>
                         </div>
                     </div>
                     <hr>
            
-             <div class="row">
+             <!-- <div class="row">
              <div class="col-sm-12 register-col">
                <p class="lead">Don't have an account yet? <a href="./register.php" class="btn btn-success">Register Here</a></p>
              </div>
-            </div>
+            </div> -->
                     <!-- <hr> -->
                     <!--Register-->
                     <!-- <div class="row">
