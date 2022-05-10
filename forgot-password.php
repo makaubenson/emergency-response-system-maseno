8c56fb7c0d53a7f4145718b61d2c220b096476be
Benson <bensonmakau2000@gmail.com>
2022-05-10 16:04:25 +0300

    updated reset password page design

--- forgot-password.php
+++ forgot-password.php
@@ -12,11 +12,21 @@ include './components/header.php';
 
 <body style="background-color: #d2d6de;">
     <div class="container">
-
-        <div class="row mt-5 login-page-row">
+    <div class="row">
+       <div class="col-md-3"></div>
+       <div class="col-md-6">
+       <div class="alert alert-warning mt-3" role="alert" style="text-align: center;">
+  ⚠ Please use the email address used to register you account.
+</div>
+       </div>
+       <div class="col-md-3"></div>
+   </div>
+  
+        <div class="row mt-1 login-page-row">
+      
             <div class="col-md-3"></div>
-            <div class="col-md-6 mt-5 login-page">
-
+            <div class="col-md-6 mt-1 login-page">
+       
                 <div class="col-md-12 mb-2 login-page-header">
                     <h2 class="text-center" style="color: #fff">Password Reset</h2>
 
@@ -33,10 +43,11 @@ include './components/header.php';
                 <form method="post" action="server.php" enctype="multipart/form-data" class="login-form">
                     <div class="form-group row">
                         <?php
-            include 'errors.php';
-            ?>
-                        <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                        <div class="col-sm-10">
+                            include 'errors.php';
+                        ?>
+       
+                        <label for="inputEmail3" class="col-sm-3 col-form-label">Email Address</label>
+                        <div class="col-sm-9">
                             <input type="email" class="form-control" id="inputEmail3" placeholder="Enter your Email Address" name="regno"
                                 required />
                         </div>
@@ -58,19 +69,7 @@ include './components/header.php';
                     </div>
                     <hr>
            
-             <!-- <div class="row">
-             <div class="col-sm-12 register-col">
-               <p class="lead">Don't have an account yet? <a href="./register.php" class="btn btn-success">Register Here</a></p>
-             </div>
-            </div> -->
-                    <!-- <hr> -->
-                    <!--Register-->
-                    <!-- <div class="row">
-                        <div class="col-sm-12 register-col">
-                            <p class="lead"> <a href="./rescue/index.php" class="btn btn-success">Rescue Team Login</a>
-                            </p>
-                        </div>
-                    </div> -->
+         
                 </form>
             </div>
         </div>
