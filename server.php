bcd1942211314c3d2f88bf2fb3a20ce5ae0dec42
Benson <bensonmakau2000@gmail.com>
2022-05-15 09:07:01 +0300

    exported db

--- server.php
+++ server.php
@@ -205,8 +205,8 @@ function send_password_reset($student_fname,$student_lname,$student_mail,$token)
   $mail->isSMTP();                                    //Send using SMTP
   $mail->Host       = 'mail.blinx.co.ke';            //Set the SMTP server to send through
   $mail->SMTPAuth   = true;                         //Enable SMTP authentication
-  $mail->Username   = 'info@blinx.co.ke';          //SMTP username
-  $mail->Password   = 'Katumo@4211';              //SMTP password
+  $mail->Username   = 'test@blinx.co.ke';          //SMTP username
+  $mail->Password   = 'blinx@2022';              //SMTP password
   $mail->SMTPSecure = 'ssl';                     //Enable implicit TLS encryption
   $mail->Port       = 465;  //465               //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
