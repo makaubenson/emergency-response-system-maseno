7af0d8b6bd33dae6196a9ee22049dda7da6a61d2
Benson <bensonmakau2000@gmail.com>
2022-05-20 13:55:23 +0300

    edited table in rescue/complete.php

--- rescue/complete.php
+++ rescue/complete.php
@@ -24,8 +24,10 @@ include './components/header.php';
       <th scope="col">Help Code</th>
       <th scope="col">Registration Number</th>
       <th scope="col">Student Name</th>
-      <th scope="col">Request Status</th>
+      <th scope="col">Description</th>
+      <!-- <th scope="col">Request Status</th> -->
       <th scope="col">Time of Request</th>
+      <th scope="col">Resolution</th>
 
     </tr>
   </thead>
@@ -33,13 +35,14 @@ include './components/header.php';
         
         <?php
         if( $_SESSION['team_id']){
-            $data_fetch_query = "SELECT request_status.helpID, request_status.ip_address, request_status.request_latitude, 
+            $data_fetch_query = "SELECT request_status.helpID, request_status.ip_address, request_status.emergency_description,request_status.request_latitude, 
             request_status.request_longitude, request_status.status, request_status.admNo, request_status.timestamp,
             student_details.firstname,student_details.lastname,student_details.regNum,
-            rescue_team_tasks.task_help_code, rescue_team_tasks.rescue_team_id, rescue_team_tasks.team_status
-             FROM ((request_status
+            rescue_team_tasks.task_help_code, rescue_team_tasks.rescue_team_id, rescue_team_tasks.team_status, success_list.student_helpcode, success_list.incident_description
+             FROM (((request_status
              INNER JOIN student_details ON request_status.admNo = student_details.regNum)
              INNER JOIN  rescue_team_tasks ON request_status.helpID =  rescue_team_tasks.task_help_code)
+             INNER JOIN  success_list ON  request_status.helpID = success_list.student_helpcode)
              WHERE rescue_team_tasks.rescue_team_id = '".$_SESSION['team_id']."' AND rescue_team_tasks.team_status ='Successful' ORDER BY timestamp ASC ";
              
             $data_result = mysqli_query($db, $data_fetch_query);
@@ -51,8 +54,10 @@ include './components/header.php';
             echo "<tr> <td>" .$row["helpID"].  "</td>";
             echo "<td>" .$row["regNum"]."</td>";
             echo "<td>" .$row["firstname"]." ".$row["lastname"]. "</td>";
-            echo "<td>" .$row["status"]."</td>";
-            echo "<td>" .$row["timestamp"]."</td> </tr>";
+            echo "<td>" .$row["emergency_description"]."</td>";
+            // echo "<td>" .$row["status"]."</td>";
+            echo "<td>" .$row["timestamp"]."</td>";
+            echo "<td>" .$row["incident_description"]."</td> </tr>";
          
             }
             
